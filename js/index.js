1629c0e0001336bc7176bfb4d1c1eb9ed1f9726c
neveennahar <neveennahar67@gmail.com>
2022-09-03 15:21:17 +0600

    catagories loaded

--- js/index.js
+++ js/index.js
@@ -11,3 +11,18 @@ const toggleSpinner = state => {
         contentDiv.style.display = 'block';
     }
 }
+
+// Active Category link
+const loadCategorizedData = (element, categoryId) => {
+    const categoryLinks = document.getElementsByClassName('category-link');
+    for (let link of categoryLinks) {
+        link.classList.remove('text-primary')
+    }
+    element.classList.add('text-primary');
+    const url = `https://openapi.programming-hero.com/api/news/category/0${categoryId}`;
+    toggleSpinner(true);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayCategorizedData(data.data))
+        .catch(error => console.log(error.message))
+}
